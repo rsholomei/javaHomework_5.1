849e9750232e020a0ab23f51dd49051432d34336
ruslan <ruslansholomey@gmail.com>
2017-03-23 02:10:00 +0200

    servlets

--- servlet/src/main/java/com/geekhub/servlets/GuestBookServlet.java
+++ servlet/src/main/java/com/geekhub/servlets/GuestBookServlet.java
@@ -9,12 +9,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "GuestBookServlet", urlPatterns = "/guestBook")
-public class GuestBookServlet extends javax.servlet.http.HttpServlet {
+public class GuestBookServlet extends HttpServlet {
 
     private GuestBookDao guestBookDao;
 
